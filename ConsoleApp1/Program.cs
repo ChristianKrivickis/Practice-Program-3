fa497aa63673aa74d9cc6af00df5a3685ff26872
Christian Krivickis <S00198911@mail.itsligo.ie>
2020-11-29 20:35:53 +0000

    Created song objects and list to hold them

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Song s1 = new Song("Beautiful People", "Ed Sheeran", 3.15, Genre.Pop);
+            Song s2 = new Song("Dance Monkey", "Tones & 1", 4.20, Genre.Dance);
+            Song s3 = new Song("Circles", "Post Malone", 3.25, Genre.Pop);
+            Song s4 = new Song("Ride It", "Regard", 3.37, Genre.Dance);
+            Song s5 = new Song("South of The Border", "Ed Sheeran", 4.26, Genre.Pop);
+
+            List<Song> playlist = new List<Song>();
+
+            playlist.Add(s1);
+            playlist.Add(s2);
+            playlist.Add(s3);
+            playlist.Add(s4);
+            playlist.Add(s5);
+
+            foreach (Song song in playlist)
+            {
+                Console.WriteLine(song);
+            }
         }
     }
 }
---
+++ ConsoleApp1/Song.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Song
+    {
+        // Properties
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public double Duration { get; set; }
+        public Genre MusicGenre { get; set; }
+
+
+        // Constructors
+        public Song(string title, string artist, double duration, Genre musicGenre)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            MusicGenre = musicGenre;
+        }
+
+        public Song(string title, string artist) : this(title, artist, 0, Genre.Other)
+        {
+
+        }
+
+        public Song() : this("Unknown", "Unknown")
+        {
+
+        }
+
+
+        // Methods
+        public override string ToString()
+        {
+            return string.Format($"{Title} {Artist} {Duration} {MusicGenre}");
+        }
+    }
+
+    public enum Genre{ Rock, Pop, Dance, Other}
+}
